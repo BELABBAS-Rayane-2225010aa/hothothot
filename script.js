4889b9ee7190072a71d24b0c639c176b976ed87e
Enzo <enzo.hourlay@etu.univ-amu.fr>
2024-02-08 12:12:12 +0100

    Add login and logout functionality

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let username = document.getElementById('registerUsername').value;
-    let password = document.getElementById('registerPassword').value;
-    localStorage.setItem(username, password);
-});
+let registerForm = document.getElementById('registerForm');
+if(registerForm != null) {
+    registerForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let username = document.getElementById('registerUsername').value;
+        let password = document.getElementById('registerPassword').value;
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    });   
+}
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    let username = document.getElementById('loginUsername').value;
-    let password = document.getElementById('loginPassword').value;
-    if(localStorage.getItem(username) === password) {
-        // Redirect to user account page
-        window.location.href = "account.html";
-    } else {
-        alert('Invalid username or password');
-    }
-});
+let loginForm = document.getElementById('loginForm');
+if(loginForm != null) {
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let username = document.getElementById('loginUsername').value;
+        let password = document.getElementById('loginPassword').value;
+        if(localStorage.getItem('username') === username && localStorage.getItem('password') === password) {
+            localSessionStorage.setItem('username', username);
+            localSessionStorage.setItem('password', password);
+            // Redirect to user account page
+            window.location.href = "account.html";
+        } else {
+            alert('Invalid username or password');
+        }
+    });
+}
