61a7c8fb8c654d6597894ce81e60478aac7dcd9c
Crespin Alexandre <126071849+CRESPIN-Alexandre-2225022aa@users.noreply.github.com>
2024-02-18 17:22:02 +0100

    feat : cache et notification fini

--- assets/script.js
+++ assets/script.js
@@ -29,6 +29,158 @@ let tempHistoryInt = new ChartHistory(histoInt, chartViewInt);
 tempSensorInt.subscribe(liveTempTrackerInt);
 tempSensorInt.subscribe(tempHistoryInt);
 
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register('/www/hothothot/sw.js').then(function(reg) {
+        // enregistrement ok
+        console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function(error) {
+        // echec de l'enregistrement
+        console.log('Registration failed with ' + error);
+    });
+}
+
+let button = document.querySelector('#notifications');
+button.addEventListener('click', function(e) {
+    Notification.requestPermission().then(function(result) {
+        if(result === 'granted') {
+            notification();
+        }
+    });
+});
+
+// Système de notification
+function notification() {
+    let TempExt = document.getElementById('tabExt').innerHTML;
+    let TempInt = document.getElementById('tabInt').innerHTML;
+    console.log(TempExt);
+    console.log(TempInt);
+    let notifTitle = "";
+    let notifBody = "";
+    let notifImg = "";
+    let options = {};
+    let notif = {};
+    if(parseInt(TempExt) > 35) {
+
+        notifTitle = "alerte : Hot Hot Hot !";
+        notifBody = 'Température extérieur : ' + TempExt + '.';
+        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
+        options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        notif = new Notification(notifTitle, options);
+    }
+    if(parseInt(TempExt) < 0) {
+
+        notifTitle = "alerte : Banquise en vue !";
+        notifBody = 'Température extérieur : ' + TempExt + '.';
+        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
+        options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        notif = new Notification(notifTitle, options);
+    }
+    if(parseInt(TempInt) > 50) {
+
+        notifTitle = "alerte : Appelez les pompiers ou arrêtez votre barbecue !";
+        notifBody = 'Température intérieur : ' + TempInt + '.';
+        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
+        options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        notif = new Notification(notifTitle, options);
+    }
+    if(parseInt(TempInt) > 22) {
+
+        notifTitle = "alerte : Baissez le chauffage !";
+        notifBody = 'Température intérieur : ' + TempInt + '.';
+        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
+        options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        notif = new Notification(notifTitle, options);
+    }
+    if(parseInt(TempInt) < 12) {
+
+        notifTitle = "alerte : montez le chauffage ou mettez un gros pull !";
+        notifBody = 'Température intérieur : ' + TempInt + '.';
+        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
+        options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        notif = new Notification(notifTitle, options);
+    }
+    if(parseInt(TempInt) < 0) {
+
+        notifTitle = "alerte : canalisations gelées, appelez SOS plombier et mettez un bonnet !";
+        notifBody = 'Température intérieur : ' + TempInt + '.';
+        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
+        options = {
+            body: notifBody,
+            icon: notifImg
+        }
+        notif = new Notification(notifTitle, options);
+    }
+    setTimeout(notification, 2000);
+}
+
+// Mise en place de l'installation de l'application
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    console.log('beforeinstallprompt event triggered');
+
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+
+
+    addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+let socket = new WebSocket('wss://ws.hothothot.dog:9502');
+
+socket.onopen = function(event) {
+    console.log("Connexion établie");
+
+    //Envoi d'un message au serveur (obligatoire)
+    socket.send("coucou !");
+
+    // au retour...
+    socket.onmessage = function(event) {
+        let data = JSON.parse(event.data);
+        tempSensorInt.notify(data.capteurs[0]);
+        tempSensorExt.notify(data.capteurs[1]);
+    }
+};
+
+/*
 setInterval(() => {
     // récupération des données de température extérieure
     fetch("https://hothothot.dog/api/capteurs/exterieur",
@@ -41,7 +193,7 @@ setInterval(() => {
         })
         .then(function(response) {
             return response.json().then(function(response) {
-                var data = response.capteurs[0];
+                let data = response.capteurs[0];
                 data.Valeur = Math.floor(Math.random() * (40 - (-10)) + (-10));
                 console.log(data);
                 tempSensorExt.notify(response.capteurs[0]);
@@ -58,13 +210,15 @@ setInterval(() => {
         })
         .then(function(response) {
             return response.json().then(function(response) {
-                var data = response.capteurs[0];
+                let data = response.capteurs[0];
                 data.Valeur = Math.floor(Math.random() * (40 - (-10)) + (-10));
                 console.log(data);
                 tempSensorInt.notify(response.capteurs[0]);
             })
         })
     }, 2000); // récupération tout les 2 secondes
+
+ */
 
 
 // Mise en place des onglets
--- sw.js
+++ sw.js
@@ -3,209 +3,123 @@
  * A laisser à la racine du projet
 */
 
-var CACHE = 'hothothot';
-
-let button = document.getElementById("notifications");
-button.addEventListener('click', function(e) {
-    Notification.requestPermission().then(function(result) {
-        if(result === 'granted') {
-            notification();
-        }
-    });
-});
+let CACHE = 'hothothot';
 
 // On installe le service worker
-self.addEventListener('install', function(evt) {  
-  evt.waitUntil(caches.open(CACHE).then(function (cache) {  
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(caches.open(CACHE).then(function (cache) {
             cache.addAll([
                 // Ajout des fichiers à mettre en cache
                 //TODO : verifier quels fichiers sont à mettre en cache
-                "/index.html",
-                "/View/documentation.html",
-                "/View/logout.html",
-                "/Controllers/CharHistory.js",
-                "/Controllers/Observable.js",
-                "/assets/script.js",
-                "/assets/style.css",
-                "/sw.js",
-      ]);  
-  }));  
+                "index.html",
+                "View/documentation.html",
+                "View/logout.html",
+                "View/profil.html",
+                "Controllers/ChartHistory.js",
+                "Controllers/Observable.js",
+                "Controllers/LiveControllers/LiveTrackerExt.js",
+                "Controllers/LiveControllers/LiveTrackerInt.js",
+                "Controllers/LiveControllers/LiveTrackerInterface.js",
+                "assets/script.js",
+                "assets/style.css",
+                "assets/images/android-chrome-192x192.png",
+                "assets/images/android-chrome-384x384.png",
+                "sw.js",
+                "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js",
+                "manifest.webmanifest",
+            ]).then(r =>(console.log('fichiers mis en cache')));
+  }));
 });
 
-// On fetch, use cache but update the entry with the latest contents  
-// from the server.  
-self.addEventListener('fetch', function(evt) {  
-    console.log('The service worker is serving the asset.');  
-    // You can use `respondWith()` to answer ASAP...  
-    evt.respondWith(fromCache(evt.request));  
-    // ...and `waitUntil()` to prevent the worker to be killed until  
+// On fetch, use cache but update the entry with the latest contents
+// from the server.
+self.addEventListener('fetch', function(evt) {
+    console.log('The service worker is serving the asset.');
+    // You can use `respondWith()` to answer ASAP...
+    evt.respondWith(fromCache(evt.request));
+    // ...and `waitUntil()` to prevent the worker to be killed until
     // the cache is updated.
-    evt.waitUntil(  
-        update(evt.request)  
-            // Finally, send a message to the client to inform it about the  
-            // resource is up to date.
-            .then(refresh)  
-    );  
-});  
-
-// Open the cache where the assets were stored and search for the requested  
-// resource. Notice that in case of no matching, the promise still resolves  
-// but it does with `undefined` as value.  
-function fromCache(request) {  
-    console.log('match cache request');  
-    return caches.open(CACHE).then(function (cache) {  
-        return cache.match(request);  
-    });  
-}  
-
-
-// Update consists in opening the cache, performing a network request and  
-// storing the new response data.  
-function update(request) {  
-    console.log('update cache');  
-    return caches.open(CACHE).then(function (cache) {  
-        return fetch(request).then(function (response) {  
-            return cache.put(request, response.clone()).then(function () {  
-                return response;  
-            });  
-        });  
-    });  
-}  
-
-// Sends a message to the clients.  
-function refresh(response) {  
-
-    return self.clients.matchAll().then(function (clients) {  
-        clients.forEach(function (client) {  
-        // Encode which resource has been updated. By including the  
-        // [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) the client can 
-        // check if the content has changed. 
-        
-                var message = {  
-                type: 'refresh',  
-                url: response.url,  
-                // Notice not all servers return the ETag header. If this is not  
-                // provided you should use other cache headers or rely on your own 
-                // means to check if the content has changed.
-                eTag: response.headers.get('ETag')  
-            };  
-            // Tell the client about the update.  
-            client.postMessage(JSON.stringify(message));  
-        });  
-    });  
-}
-
-// Système de notification
-function notification() {
-    var TempExt = document.getElementById('tabExt').innerHTML;
-    var TempInt = document.getElementById('tabInt').innerHTML;
-    console.log(TempExt);
-    console.log(TempInt);
-    var notifTitle = "";
-    var notifBody = "";
-    var notifImg = "";
-    var options = {};
-    var notif = {};
-    if(parseInt(TempExt) > 35) {
-
-		 notifTitle = "alerte : Hot Hot Hot !";
-		 notifBody = 'Température extérieur : ' + TempExt + '.';
-		 notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
-		 options = {
-            body: notifBody,
-            icon: notifImg
-        }
-        notif = new Notification(notifTitle, options);
-    }
-    if(parseInt(TempExt) < 0) {
-
-        notifTitle = "alerte : Banquise en vue !";
-        notifBody = 'Température extérieur : ' + TempExt + '.';
-        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
-        options = {
-            body: notifBody,
-            icon: notifImg
-        }
-        notif = new Notification(notifTitle, options);
-    }
-    if(parseInt(TempInt) > 50) {
-
-        notifTitle = "alerte : Appelez les pompiers ou arrêtez votre barbecue !";
-        notifBody = 'Température intérieur : ' + TempInt + '.';
-        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
-        options = {
-            body: notifBody,
-            icon: notifImg
-        }
-        notif = new Notification(notifTitle, options);
-    }
-    if(parseInt(TempInt) > 22) {
-
-        notifTitle = "alerte : Baissez le chauffage !";
-        notifBody = 'Température intérieur : ' + TempInt + '.';
-        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
-        options = {
-            body: notifBody,
-            icon: notifImg
-        }
-        notif = new Notification(notifTitle, options);
-    }
-    if(parseInt(TempInt) < 12) {
-
-        notifTitle = "alerte : montez le chauffage ou mettez un gros pull !";
-        notifBody = 'Température intérieur : ' + TempInt + '.';
-        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
-        options = {
-            body: notifBody,
-            icon: notifImg
-        }
-        notif = new Notification(notifTitle, options);
-    }
-    if(parseInt(TempInt) < 0) {
-
-        notifTitle = "alerte : canalisations gelées, appelez SOS plombier et mettez un bonnet !";
-        notifBody = 'Température intérieur : ' + TempInt + '.';
-        notifImg = '/www/hothothot/assets/images/android-chrome-192x192.png';
-        options = {
-            body: notifBody,
-            icon: notifImg
-        }
-        notif = new Notification(notifTitle, options);
-    }
-    setTimeout(notification, 2000);
-}
-
-//On génére un nombre aléatoire pour la démo  
-function getRandomInt(max) {  
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-//partie javascript pour l'ajout à l'écran d'accueil
-let deferredPrompt;  
-const addBtn = document.querySelector('.add-button');  
-addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {  
-    // Prevent Chrome 67 and earlier from automatically showing the prompt  
-      e.preventDefault();  
-      // Stash the event so it can be triggered later.  
-      deferredPrompt = e;  
-      // Update UI to notify the user they can add to home screen  
-      addBtn.style.display = 'block';  
-  
-      addBtn.addEventListener('click', (e) => {  
-          // hide our user interface that shows our A2HS button  
-          addBtn.style.display = 'none';  
-          // Show the prompt  
-          deferredPrompt.prompt();  
-          // Wait for the user to respond to the prompt  
-          deferredPrompt.userChoice.then((choiceResult) => {  
-                if (choiceResult.outcome === 'accepted') {  
-                    console.log('User accepted the A2HS prompt');  
-              } else {  
-                    console.log('User dismissed the A2HS prompt');  
-              }  
-              deferredPrompt = null;  
-          });  
-    });  
+    evt.waitUntil(
+        update(evt.request)
+            // Finally, send a message to the client to inform it about the
+            // resource is up-to-date.
+            .then(refresh)
+    );
 });
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves,
+// but it does with `undefined` as value.
+function fromCache(request) {
+    console.log('match cache request');
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request);
+    });
+}
+
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+    console.log('update cache');
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response.clone()).then(function () {
+                return response;
+            });
+        });
+    });
+}
+
+// Sends a message to the clients.
+function refresh(response) {
+
+    return self.clients.matchAll().then(function (clients) {
+        clients.forEach(function (client) {
+        // Encode which resource has been updated. By including the
+        // [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) the client can
+        // check if the content has changed.
+
+                let message = {
+                type: 'refresh',
+                url: response.url,
+                // Notice not all servers return the ETag header. If this is not
+                // provided you should use other cache headers or rely on your own
+                // means to check if the content has changed.
+                eTag: response.headers.get('ETag')
+            };
+            // Tell the client about the update.
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
+
+// //partie javascript pour l'ajout à l'écran d'accueil
+// let deferredPrompt;
+// const addBtn = document.querySelector('.add-button');
+// addBtn.style.display = 'none';
+//
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//       e.preventDefault();
+//       // Stash the event, so it can be triggered later.
+//       deferredPrompt = e;
+//       // Update UI to notify the user they can add to home screen
+//       addBtn.style.display = 'block';
+//
+//       addBtn.addEventListener('click', (e) => {
+//           // hide our user interface that shows our A2HS button
+//           addBtn.style.display = 'none';
+//           // Show the prompt
+//           deferredPrompt.prompt();
+//           // Wait for the user to respond to the prompt
+//           deferredPrompt.userChoice.then((choiceResult) => {
+//                 if (choiceResult.outcome === 'accepted') {
+//                     console.log('User accepted the A2HS prompt');
+//               } else {
+//                     console.log('User dismissed the A2HS prompt');
+//               }
+//               deferredPrompt = null;
+//           });
+//     });
+// });
